bce0a03b3085e7eaeda3008fcff73790e9a64930
Danial Sajjadi <danial.sajadi@gmail.com>
2022-01-21 22:12:49 +0100

    the pong game on python

---
+++ Python/pong_game/main.py
@@ -0,0 +1,57 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("--- PONG GAME ---")
+
+screen.tracer(0) # to turn off the animation tracer
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+scoreboard = Scoreboard()
+
+
+screen.listen()
+
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    #collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # it needs to be bounce
+        ball.bounce_y()
+
+
+    # collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
+        time.sleep(0.1 * 0.1)
+
+
+    # miss the ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+screen.exitonclick()
